b6f86ed9a776c24677376579450e1aa2c90a97d5
letaunahn <letuananh28072002@gmail.com>
2024-06-10 11:11:22 +0700

    Check whether admin

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -9,6 +9,16 @@ export const isAuthenticateUser = async (req, res, next) => {
       .json({ success: false, message: "Please to access this resource" });
   }
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = await userModel.findById(decodedData._id);
+  console.log(decodedData)
+  req.user = await userModel.findById(decodedData.id);
   next();
+};
+
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if(!roles.includes(req.user.role)){
+        return res.status(403).json({success: false, message: `Role: ${req.user.role} cannot access to this resource`})
+    }
+    next()
+  }
 };
