91ab8e725dc6d386ebe0b84f863c9394f168c2ba
letaunahn <letuananh28072002@gmail.com>
2024-06-10 11:15:46 +0700

    set up nodemailer to send email to user

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,21 @@
+import nodemailer from 'nodemailer'
+export const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port: process.env.SMPT_PORT,
+        secure: false,
+        service: 'Gmail',
+        auth: {
+            user: process.env.SMPT_MAIL,
+            pass: process.env.SMPT_PASSWORD
+        }
+    })
+
+    const mailOptions = {
+        from: process.env.SMPT_MAIL,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+    }
+    await transporter.sendMail(mailOptions)
+}
