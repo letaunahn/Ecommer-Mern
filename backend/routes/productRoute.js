5fae22296a2a5ce2f9978592d8b5f80a4f697ed3
letaunahn <letuananh28072002@gmail.com>
2024-06-10 11:13:34 +0700

    Update route for product and user

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,36 @@
-import express from 'express'
-import { createProduct, deleteProduct, getAllProducts, getProductDetails, updateProduct } from '../controllers/productController.js'
-import { isAuthenticateUser } from '../middleware/auth.js'
+import express from "express";
+import {
+  createProduct,
+  createProductReview,
+  deleteProduct,
+  getAllProducts,
+  getProductDetails,
+  updateProduct,
+} from "../controllers/productController.js";
+import { authorizeRoles, isAuthenticateUser } from "../middleware/auth.js";
 
-const productRouter = express.Router()
+const productRouter = express.Router();
 
-productRouter.get('/all', isAuthenticateUser, getAllProducts)
-productRouter.post('/add', createProduct)
-productRouter.put('/:id', updateProduct)
-productRouter.delete('/:id', deleteProduct)
-productRouter.get('/:id', getProductDetails)
+productRouter.get("/all", isAuthenticateUser, getAllProducts);
+productRouter.post(
+  "/add",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  createProduct
+);
+productRouter.put(
+  "/:id",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  updateProduct
+);
+productRouter.delete(
+  "/:id",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  deleteProduct
+);
+productRouter.get("/:id", getProductDetails);
+productRouter.post("/review", isAuthenticateUser, createProductReview)
 
-export default productRouter
+export default productRouter;
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,54 @@
-import express from 'express'
-import { loginUser, registerUser } from '../controllers/userController.js'
+import express from "express";
+import {
+  deleteUser,
+  forgotPassword,
+  getAllUsers,
+  getSingleUser,
+  getUserDetails,
+  loginUser,
+  logoutUser,
+  registerUser,
+  resetPassword,
+  updatePassword,
+  updateProfile,
+  updateUserRole,
+} from "../controllers/userController.js";
+import { authorizeRoles, isAuthenticateUser } from "../middleware/auth.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.post('/register', registerUser)
-userRouter.post('/login', loginUser)
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", logoutUser);
+userRouter.post("/password/forgot", forgotPassword);
+userRouter.put("/password/reset/:token", resetPassword);
+userRouter.get("/myprofile", isAuthenticateUser, getUserDetails);
+userRouter.put("/myprofile/update", isAuthenticateUser, updateProfile);
+userRouter.put("/updatepassword", isAuthenticateUser, updatePassword);
+userRouter.put("/updateprofile", isAuthenticateUser, updateProfile);
+userRouter.get(
+  "/admin/users",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  getAllUsers
+);
+userRouter.get(
+  "/admin/user/:id",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  getSingleUser
+);
+userRouter.put(
+  "/admin/user/:id",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  updateUserRole
+);
+userRouter.delete(
+  "/admin/user/:id",
+  isAuthenticateUser,
+  authorizeRoles("admin"),
+  deleteUser
+);
 
-export default userRouter
+export default userRouter;
